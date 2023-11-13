ccd0d247b337eb15ea7b6ed684ceecc0483caada
bo <xjsoni@gmail.com>
2023-11-13 09:14:31 +0800

    Fix

--- model/coin.go
+++ model/coin.go
@@ -4,7 +4,16 @@ import "gorm.io/gorm"
 
 type Coin struct {
 	gorm.Model
-	Name    string // 名称
-	Network string // 网络
-	Logo    string // 图标
+	Name    string      // 名称
+	Network CoinNetwork // 网络
+	Logo    string      // 图标
 }
+
+type CoinNetwork string
+
+const (
+	CoinNetworkETH = CoinNetwork("eth")
+	CoinNetworkBSC = CoinNetwork("bsc")
+	CoinNetworkARB = CoinNetwork("arb")
+	CoinNetworkTRX = CoinNetwork("trx")
+)
